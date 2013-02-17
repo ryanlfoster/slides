98d2a3a78cbfc49de53bfce2208bc86cc72a1d78
AJ Marino <aj@ajmarino.com>
2013-02-16 21:11:30 -0500

    Adds shims and paths
    - Adds shim for Backbone with Underscore and jQuery
    - Adds path for Backbone

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,13 @@
 require.config({
-  shim: {
-  },
+	shim: {
+		"backbone" : {
+			deps     : ['../components/underscore/underscore', 'jquery'],
+			exports : 'Backbone'
+		}
+	},
 
-  paths: {
-    jquery: 'vendor/jquery.min'
-  }
-});
- 
-require(['app'], function(app) {
-  // use app here
-  console.log(app);
+	paths: {
+		jquery   : 'vendor/jquery.min',
+		backbone : '../components/backbone/backbone'
+	}
 });
