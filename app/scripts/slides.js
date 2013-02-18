b7486803b2cac38b7bdb34835a7d9b1a50e5a8ce
AJ Marino <aj@ajmarino.com>
2013-02-17 20:28:07 -0500

    Adds bullets
    - Adds option to display bullet lists
    - Adds separate render for headings

--- app/scripts/slides.js
+++ app/scripts/slides.js
@@ -2,7 +2,10 @@ window.slides = [
 	{ title: "My Presentation" },
 	{ image: "http://placekitten.com/800/300" },
 	{ title: "Hello my name is AJ" },
-	{ title: "I work for Reflexions Data" },
+	{ 
+		title: 'Why this matters',
+		bullets: ['Point 1', 'Point 2', 'Point 3']
+	},
 	{ title: "Lorem ipsum dolor sit amet, consectetur adipisicing." },
 	{ title: "The End" }
 ]
--- app/scripts/views/slide.js
+++ app/scripts/views/slide.js
@@ -6,19 +6,41 @@ define(['backbone'], function(Backbone) {
 		render : function () {
 			if ( this.model.get('image') ) {
 				this.renderImage();
+			} else if (this.model.get('bullets') ) {
+				this.renderBullets();
+			} else {
+				this.renderHeading();
 			}
 
+			return this;
+		},
+
+		renderHeading: function () {
 			this.$el.append(
 				'<h1 class=' + this.model.get('size') + '>' + this.model.get('title') + '</h1>'
 			);
-
-			return this;
 		},
 
 		renderImage: function () {
 			this.$el
 				.addClass( 'image' )
 				.append('<img src=' + this.model.get('image') + '>');
+		},
+
+		renderBullets: function () {
+			var el = this.$el;
+
+			el.addClass('bullets')
+
+			if ( this.model.get( 'title' )) {
+				el.append('<h1>' + this.model.get('title') + '</h1>')
+			}
+
+			el.append([
+					'<ul>',
+						'<li>' + this.model.get('bullets').join('</li><li>'),
+					'</ul>'
+				].join(' '));
 		}
 	});
 
