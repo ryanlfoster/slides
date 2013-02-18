f6913c3e50bf6e1d5494c5bcb7e97ede710453bb
AJ Marino <aj@ajmarino.com>
2013-02-17 22:20:12 -0500

    Moves slides.js to /app
    - Moves slides.js from /app/scripts to /app

---
+++ app/slides.js
@@ -0,0 +1,22 @@
+window.slides = [
+	{ title: "My Presentation" },
+
+	{ image: "http://placekitten.com/800/300" },
+
+	{ 
+		title   : 'Why this matters',
+		bullets : ['Point 1', 'Point 2', 'Point 3']
+	},
+
+	{ title: "Lorem ipsum dolor sit amet, consectetur adipisicing." },
+
+	{
+		quote : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam et quasi sed amet totam sapiente!",
+		cite  : "John Doe"
+	},
+
+	{
+		title   : "Some CSS code here",
+		snippet : 'snippets/ex1.css'
+	}
+]
