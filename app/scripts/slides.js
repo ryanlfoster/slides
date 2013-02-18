d55b800674ec5d34a573256df1adee70c99276de
AJ Marino <aj@ajmarino.com>
2013-02-17 20:15:45 -0500

    Adds image render option
    - Adds option to have an image on a slide

--- app/scripts/slides.js
+++ app/scripts/slides.js
@@ -1,5 +1,6 @@
 window.slides = [
 	{ title: "My Presentation" },
+	{ image: "http://placekitten.com/800/300" },
 	{ title: "Hello my name is AJ" },
 	{ title: "I work for Reflexions Data" },
 	{ title: "Lorem ipsum dolor sit amet, consectetur adipisicing." },
